4ec4e11c65bd25c6bec01de40e9b3eb84747702a
KVRTeJ <bb.sssss@inbox.ru>
2023-09-15 12:09:53 +0700

    ООП Начинаю делать ввод данных

--- Alg/#1 alg/#1 alg/main.cpp
+++ Alg/#1 alg/#1 alg/main.cpp
@@ -9,9 +9,27 @@
 
 #include <iostream>
 
+void PrintMatr(int(&a)[5][5], int c, int b)
+{
+    for(int i = 0; i < c; i++) {
+        for(int j = 0; j < b; j++)
+            std::cout << a[i][j];
+        std::cout << std::endl;
+    }
+}
+
 int main() {
     
-    std::cout << "Hello World!" << std::endl;
+    //numberCities - кол-во городов, startCity - начальный город
+    //priseMatr - матрица стоимости
+    int NumberOfCities, StartingCity, **PriseMatr;
+
+    std::cout << "Кол-во городов - ";
+    std::cin >> NumberOfCities;
+    std::cout << "Стартовый город - ";
+    std::cin >> StartingCity;
+    
+    **priseMatr = new int*[NumberOfCities*NumberOfCities];
     
     return 0;
 }
