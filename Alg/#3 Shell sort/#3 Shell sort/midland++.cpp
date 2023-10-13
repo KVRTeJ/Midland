7ba371937edc7ddf643d3c8e2db99b0d364a3ef9
KVRTeJ <bb.sssss@inbox.ru>
2023-10-13 11:21:08 +0700

    АИСД Создан проект по 3 лабороторной

---
+++ Alg/#3 Shell sort/#3 Shell sort/midland++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "midland++.hpp"
+
+HelloWorld::HelloWorld() {
+    
+    std::cout << "Hello World!" << std::endl;
+    
+}
+
+HelloWorld::~HelloWorld() {
+    
+    std::cout << "Bye World! :(" << std::endl;
+    
+}
---
+++ Alg/#3 Shell sort/#3 Shell sort/midland++.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class HelloWorld {
+    
+public:
+    HelloWorld();
+    ~HelloWorld();
+private:
+    bool True = true;
+    
+};
