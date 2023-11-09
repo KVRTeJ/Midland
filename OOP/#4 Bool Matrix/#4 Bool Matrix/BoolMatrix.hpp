3b5a92a36642ae61c78966798658c493333d3a04
KVRTeJ <bb.sssss@inbox.ru>
2023-11-09 14:54:33 +0700

    ООП Объявление методов, полей и перегрузок

--- OOP/#4 Bool Matrix/#4 Bool Matrix/BoolMatrix.hpp
+++ OOP/#4 Bool Matrix/#4 Bool Matrix/BoolMatrix.hpp
@@ -8,10 +8,34 @@ class BoolMatrix {
 public:
     class BoolVector;
 public:
-    BoolMatrix() { std::cout << "Hello World" << std::endl; }
+    BoolMatrix(const int rowSize = 3, const int columnSize = 3, const bool value = false) {
+        std::cout << "Hello World" << std::endl;
+    }
+    BoolMatrix(const char** matrix);
+    BoolMatrix(const BoolMatrix& other);
+    BoolMatrix(BoolMatrix&& other);
+    ~BoolMatrix();
+    
+    int getRowSize() const;
+    int getColumnSize() const;
+    
+    bool swap();
+    void scan();
+    void print() const;
+    
+    int weight() const;
+    
+    
+    BoolMatrix& operator = (const BoolMatrix& other);
+    BoolMatrix& operator = (BoolMatrix&& other);
+    
     
 private:
     BoolVector* matrix = nullptr;
     const int rowSize = 0;
     const int columnSize = 0;
 };
+
+std::istream& operator << (std::istream& stream, BoolMatrix& other);
+std::ostream& operator << (std::ostream& stream, BoolMatrix& other);
+
--- OOP/#4 Bool Matrix/#4 Bool Matrix/main.cpp
+++ OOP/#4 Bool Matrix/#4 Bool Matrix/main.cpp
@@ -2,8 +2,7 @@
 
 #include "BoolMatrix.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    BoolMatrix matrix;
     return 0;
 }
